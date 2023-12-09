345b017e17e2b529ca94477b35d69ebf364adebb
Knightwood <33772264+Knightwood@users.noreply.github.com>
2023-12-09 13:55:16 +0800

    Update build.gradle.kts

--- ActivityExt/build.gradle.kts
+++ ActivityExt/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.kiylx.libx.activity_ktx"
-    compileSdk = 32
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
@@ -26,11 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 afterEvaluate {
